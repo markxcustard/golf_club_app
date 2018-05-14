1ce5e9314166d68a932ff9ca0fb17327aca6dfca
Mark Custard <markc1772@gmail.com>
2018-05-14 18:24:19 -0500

    Fixed uninitialized constant error in FairwayWoods controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   get "/drivers" => "drivers#index"
   get "/drivers/:id" => "drivers#show"
-  get "/fairwaywoods" => "fairwaywoods#index"
-  get "/fairwaywoods/:id" => "fairwaywoods#show"
+
+  get "/fairwaywoods" => "fairway_woods#index"
+  get "/fairwaywoods/:id" => "fairway_woods#show"
+
   get "/hybrids" => "hybrids#index"
   get "/hybrids/:id" => "hybrids#show"
+
   get "/irons" => "irons#index"
   get "/irons/:id" => "irons#show"
+
   get "/wedges" => "wedges#index"
   get "/wedges/:id" => "wedges#show"
+
   get "/putters" => "putters#index"
   get "/putters/:id" => "putters#show"
 end
